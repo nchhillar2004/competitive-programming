a4f9b507e59c849b5fd3457181f47cd4dcf60292
nchhillar2004 <115921678+nchhillar2004@users.noreply.github.com>
2025-02-15 23:47:08 +0530

    71A Way Too Long Words (Java)

---
+++ codeforces/0000-1000/71A Way Too Long Words/Main.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String[] strs = new String[n];
+        int i = 0;
+
+        while (sc.hasNext()){
+            strs[i] = sc.next();
+            i++;
+        }
+
+        for (String str : strs){
+            int len = str.length();
+            if (len > 10){
+                String result = String.valueOf(str.charAt(0)) + (len-2) + String.valueOf(str.charAt(len-1));
+                System.out.println(result);
+            }else{
+                System.out.println(str);
+            }
+        }
+    }
+}
